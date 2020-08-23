a4a1ed7f420c1244247e96f1ec611f720e722319
nima-19 <nima.thakare@gmail.com>
2020-08-23 07:01:58 +0530

    Star Programs

--- cipher.rb
+++ cipher.rb
@@ -4,12 +4,13 @@ end
 
 def convert(char,shift)
 	if (char>=65 && char<=90)
-		return char(((((char+shift-65)% 26)+26)%26)+65)
-	else if (char>=97 && char<=122)
-		return char(((((char+shift-97)% 26)+26)%26)+97)
+		return (((((char+shift-65)% 26)+26)%26)+65)
+	elsif (char>=97 && char<=122)
+		return (((((char+shift-97)% 26)+26)%26)+97)
 	else
-		return char
+		return
 	end
+
 end
 loop do 
 	puts "Enter your string to be coded?"
---
+++ star_program2.rb
@@ -0,0 +1,5 @@
+n=5
+while n>=1
+	puts "*" * n 
+	n = n-1 
+end
