09093983c5f8d2ccc238d9311549eec9184f0941
nima-19 <nima.thakare@gmail.com>
2020-08-28 09:36:01 +0530

    Method within method call

--- Viking_class.rb
+++ Viking_class.rb
@@ -1,30 +1,31 @@
 class Viking
+	@@starting_health = 8
 	attr_accessor :name
 	attr_writer :age,:enemy
 	attr_reader :strength 
 	
-	def initialize(name,age,health,strength)
+	def initialize(name,age,health)	#initialize
 		@name = name
 		@age = age
 		@health = health
-		@strength = strength
+		@strength = @@starting_health
 	end
 
-	def attack(enemy)
+	def attack(enemy)			#instance variables also denoted by self
 		self.enemy = enemy
 		#puts @enemy
 	end
 	
 	def health
-		@health  
-		#puts @health
+		@health  		#getter
+		#puts @health		
 	end
 	
-	def health=(new_health)
+	def health=(new_health)		#setter
 		@health = new_health
 	end
 
-	def take_damage
+	def take_damage			#method inside the method call
 		self.shout("Ouch!!!")
 	end
 	
@@ -32,7 +33,7 @@ class Viking
 		 str
 	end
 end
-oleg = Viking.new("oleg",19,100,8)
+oleg = Viking.new("oleg",19,100)
 puts oleg.attack("oleg")
 puts oleg.health
 puts oleg.health=(85)
